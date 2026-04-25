fd7196ae61a3dd563d0909d9532d802b095b7607
VelveT-git <velvetfromohio@gmail.com>
2026-04-26 00:21:19 +0530

    Update questions.py

--- questions.py
+++ questions.py
@@ -17,5 +17,17 @@ questions = [
         "category": "Math",
         "question": "What three-digit number can be made from the digits 2, 3, 5, and 7, such that no two digits of the three-digit number are the same and the three-digit number is divisible by each of the digits in it?",
         "answer": "735"
+},
+    {
+        "type": "NAT",
+        "category": "Math",
+        "question": "27 cubes of edge 10 cm are arranged to create a larger cube. If the cubes at the eight corners are replaced with spheres of diameter 10 cm, what is the minimum number of cubes that do not touch any of the spheres?",
+        "answer": "7"
+},
+    {
+        "type": "NAT",
+        "category": "Math",
+        "question": "A painter mixes white and black paints to create different shades in two different buckets. He prepares 10 kg in one bucket with 20% black paint in it. He then prepares 20 kg of another shade in the second bucket with 20% black paint in it. He pours the second bucket into the first one. What is the percentage of black paint in the mixture?",
+        "answer": "20"
 }
 ]
