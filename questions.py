3eca58c008c0c4a6b663164d56b00086b1a85872
VelveT-git <velvetfromohio@gmail.com>
2026-04-26 00:13:58 +0530

    Update questions.py

--- questions.py
+++ questions.py
@@ -11,5 +11,11 @@ questions = [
         "category": "Math",
         "question": "With a straight cut you can slice a circular piece of paper into two pieces. A second cut that crosses the first cut will produce a maximum of four pieces. What is the maximum number of pieces that you can get with a total of four straight cuts?",
         "answer": "11"
-    }
+    },
+     {
+        "type": "NAT",
+        "category": "Math",
+        "question": "What three-digit number can be made from the digits 2, 3, 5, and 7, such that no two digits of the three-digit number are the same and the three-digit number is divisible by each of the digits in it?",
+        "answer": "735"
+}
 ]
