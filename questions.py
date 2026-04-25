0c71388ad64690b86611173a98c9e221249a1afb
VelveT-git <velvetfromohio@gmail.com>
2026-04-26 00:09:42 +0530

    Update questions.py

--- questions.py
+++ questions.py
@@ -5,7 +5,7 @@ questions = [
         "question": "Q: Betty bought some butter but the butter was too bitter so she put a little gummy to make the bitter butter yummy. In the above sentence, if you replace every letter 't' with letter 'b', what is the total number of instances in which the same letter appears at least twice in a word?",
         "image": "images/q1.png",
         "answer": "7"
-    }
+    },
      {
         "type": "NAT",
         "category": "Math",
